87612bd89ccc65f63f83b6a532ef24c509924e1e
Pedrocod <pedro.hsoares131@gmail.com>
2021-10-20 12:49:10 -0300

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
-let message = "";
+ let message = "";
 
 
 app.set("view engine", "ejs");
@@ -15,7 +15,7 @@ const Jogos = require("./models/jogos");
 
 app.get("/", async (req, res) => {
   const jogo = await Jogos.findAll();
-
+  
   setTimeout(() => {
     message = "";
   }, 1000);
@@ -25,85 +25,51 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.get("/detalhes/:id", async (req, res) => {
-  const jogo = await Jogos.findByPk(req.params.id);
-  res.render("detalhes", {
-    jogo,
+  app.get("/detalhes/:id", async (req, res) => {
+    const jogo = await Jogos.findByPk(req.params.id);
+    res.render("detalhes", {
+      jogo,
+    });
   });
-});
 
 app.get("/cadastro", (req, res) => {
-  res.render("cadastro", {
+  res.render("cadastro",{
     message,
   });
 });
-app.get("/sobre", (req, res) => {
+app.get("/sobre",(req,res)=>{
   res.render("sobre")
 });
 
 app.post("/new", async (req, res) => {
-  const {
+  const { nome, descricao, imagem, genero } = req.body;
+
+  const jogo = await Jogos.create({
     nome,
     descricao,
     imagem,
-    genero
-  } = req.body;
-  if (!nome) {
-    res.render("cadastro", {
-      message: "Nome é obrigatório",
-    });
-  } else if (!imagem) {
-    res.render("cadastro", {
-      message: "Imagem é obrigatório",
-    });
-  } else {
-    try {
-      const jogo = await Jogos.create({
-        nome,
-        descricao,
-        imagem,
-        genero
-      });
-      jogo
-      message = `O Jogo ${jogo.nome} Foi Cadastrado com sucesso!!!`
-      res.redirect("/");
-    } catch (err) {
-      console.log(err);
-
-      res.render("criar", {
-        message: "Ocorreu um erro ao cadastrar o Filme!",
-      });
-    }
-  }
+    genero,
+  });
+  message = `O Jogo ${jogo.nome} foi criado!!!`
+  res.redirect("/");
 });
 
 
-app.get("/editar/:id", async (req, res) => {
-
+app.get("/editar/:id", async (req, res) => {  
+  
   const jogo = await Jogos.findByPk(req.params.id)
-
-  if (!filme) {
-    res.render("editar", {
-      filme,
-      message: "Filme não encontrado!",
-    });
-  }
-
-  res.render("editar", {
-    filme,
-    message
+  
+  res.render("editar", {    
+    jogo,
+    message,
   });
+
 });
 
 app.post("/editar/:id", async (req, res) => {
   const jogo = await Jogos.findByPk(req.params.id);
 
-  const {
-    nome,
-    descricao,
-    imagem,
-    genero
-  } = req.body;
+  const { nome, descricao, imagem, genero } = req.body;
 
   jogo.nome = nome;
   jogo.descricao = descricao;
@@ -112,18 +78,18 @@ app.post("/editar/:id", async (req, res) => {
 
   const jogoEditado = await jogo.save();
   jogoEditado,
-
+message = `O jogo ${jogo.nome} foi Editado com sucesso!!`,
   res.redirect("/");
 });
-message = `O jogo ${jogo.nome} foi Editado com sucesso!!`,
-  app.get("/deletar/:id", async (req, res) => {
-    const jogo = await Jogos.findByPk(req.params.id);
 
-    res.render("deletar", {
-      jogo,
-      message
-    });
+app.get("/deletar/:id", async (req, res) => {
+  const jogo = await Jogos.findByPk(req.params.id);
+
+  res.render("deletar", {
+    jogo,
+    message
   });
+});
 app.post("/deletar/:id", async (req, res) => {
   const jogo = await Jogos.findByPk(req.params.id);
 
